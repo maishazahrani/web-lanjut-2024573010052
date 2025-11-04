7195c93a797814f8dcd4f7836420c4c207f6ad10
Maisha Zahrani <zahranimaisha@gmail.com>
2025-11-04 15:48:41 +0700

    push

---
+++ projects/complex-relationships/app/Models/User.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use App\Models\Profile;
+use App\Models\Post;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+    // Relasi One-to-One dengan Profile
+    public function profile()
+    {
+        return $this->hasOne(Profile::class);
+    }
+
+    // Relasi One-to-Many dengan Post
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+}
